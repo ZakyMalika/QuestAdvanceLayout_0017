7008cd861f78d0e3c5cdb9b8e91fe184d24087b6
ZakyMalika <zakymalika0987@gmail.com>
2025-10-25 01:13:08 +0700

    menambahkan gambar dan membuat fungsi Image agar gambarnya bisa muncul

--- app/src/main/java/com/example/uipertama/UIPertama.kt
+++ app/src/main/java/com/example/uipertama/UIPertama.kt
@@ -1,5 +1,6 @@
 package com.example.uipertama
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -51,7 +53,12 @@ fun ActPertama(modifier: Modifier){
                 verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier.padding(all = 10.dp)
             ){
+                val gambar = painterResource(id = R.drawable.me)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
 
+                )
             }
         }
     }
