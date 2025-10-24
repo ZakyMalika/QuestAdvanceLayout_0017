dcdfb0e73dbb4fac3303204d65e046df09fd0296
ZakyMalika <zakymalika0987@gmail.com>
2025-10-25 01:21:10 +0700

    membuat fungsi column untuk memasukkan text di dalam Card

--- app/src/main/java/com/example/uipertama/UIPertama.kt
+++ app/src/main/java/com/example/uipertama/UIPertama.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -65,6 +66,21 @@ fun ActPertama(modifier: Modifier){
                 )
 
                 Spacer(modifier = Modifier.width(width = 30.dp  ))
+
+                Column(){
+                    Text(
+                        text = stringResource(id = R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color =Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
+                    )
+                    Text(
+                        text = stringResource(id = R.string.alamat),
+                        fontSize = 20.sp,
+                        color = Color.Blue
+                    )
+                }
             }
         }
     }
