4c1dce18de80a1fe937444ad07d92df8872c8066
ZakyMalika <zakymalika0987@gmail.com>
2025-10-25 09:55:04 +0700

    menambahkan 4 gambar

--- app/src/main/java/com/example/uipertama/UIKedua.kt
+++ app/src/main/java/com/example/uipertama/UIKedua.kt
@@ -2,12 +2,15 @@ package com.example.uipertama
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
@@ -24,8 +27,55 @@ fun ActKedua(modifier: Modifier){
             painter = gambar1,
             contentDescription = null,
             modifier = Modifier
-                .size(size = 100.dp)
+                .size(size = 200.dp)
                 .padding(all = 2.dp)
+                .clip(RoundedCornerShape(16.dp))
         )
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(all = 10.dp)
+        ) {
+            Column() {
+                val gambar2 = painterResource(id = R.drawable.instagram)
+                Image(
+                    painter = gambar2,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(size = 200.dp)
+                        .padding(all = 2.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                )
+                val gambar3 = painterResource(id = R.drawable.x)
+                Image(
+                    painter = gambar3,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(size = 200.dp)
+                        .padding(all = 2.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                )
+
+                val gambar4 = painterResource(id = R.drawable.facebook)
+                Image(
+                    painter = gambar4,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(size = 200.dp)
+                        .padding(all = 2.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                )
+
+                val gambar5 = painterResource(id = R.drawable.tiktok)
+                Image(
+                    painter = gambar5,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(size = 200.dp)
+                        .padding(all = 2.dp)
+                        .clip(RoundedCornerShape(16.dp))
+                )
+            }
+        }
     }
 }
